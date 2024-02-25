b4f87942bcacb92dc2983c70e26b8d41be2dcc07
CIPRIANO <ciprianomiguelangel.dev@gmail.com>
2024-02-24 23:47:30 -0300

    BUILD: Browser server connection

--- src/server.js
+++ src/server.js
@@ -1,9 +1,19 @@
 import express from 'express'
 import cookieParser from 'cookie-parser'
+import morgan from 'morgan'
+import path from 'path'
+import { fileURLToPath } from 'url'
+
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = path.dirname(__filename)
 
 const app = express()
 
+app.use(morgan('dev'))
+
 app.use(cookieParser())
+
+app.use(express.static(path.join(__dirname, 'public')))
 
 app.get('/', (req, res) => {
 	res.send('Landing')
